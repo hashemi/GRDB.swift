322aa0cd5f6ac4db34f9b7b478fcca9203c10d70
Gwendal Roué <gr@pierlis.com>
2017-06-26 08:31:40 +0200

    HasManyThroughAssociationHasManyBelongsToHavingAnnotationTests

--- GRDB/QueryInterface/Association/HasManyThrough/Request/HasManyThroughAnnotationHavingRequest.swift
+++ GRDB/QueryInterface/Association/HasManyThrough/Request/HasManyThroughAnnotationHavingRequest.swift
@@ -16,7 +16,62 @@ extension HasManyThroughAnnotationHavingRequest : TypedRequest {
     public typealias RowDecoder = MiddleAssociation.LeftAssociated
     
     public func prepare(_ db: Database) throws -> (SelectStatement, RowAdapter?) {
-        fatalError("Not implemented")
+        var leftQualifier = SQLSourceQualifier()
+        var middleQualifier = SQLSourceQualifier()
+        var rightQualifier = SQLSourceQualifier()
+        
+        // SELECT * FROM left ... -> SELECT left.* FROM left ...
+        let leftQuery = leftRequest.query.qualified(by: &leftQualifier)
+        
+        // SELECT * FROM middle ... -> SELECT middle.* FROM middle ...
+        let middleQuery = annotationHavingExpression.annotation.association.middleAssociation.rightRequest.query.qualified(by: &middleQualifier)
+        
+        // SELECT * FROM right ... -> SELECT right.* FROM right ...
+        let rightQuery = annotationHavingExpression.annotation.association.rightAssociation.rightRequest.query.qualified(by: &rightQualifier)
+        
+        // ... FROM left LEFT JOIN middle LEFT JOIN right
+        guard let leftSource = leftQuery.source else { fatalError("Support for sourceless joins is not implemented") }
+        guard let middleSource = middleQuery.source else { fatalError("Support for sourceless joins is not implemented") }
+        guard let rightSource = rightQuery.source else { fatalError("Support for sourceless joins is not implemented") }
+        
+        let joinedSource = try SQLSource.joined(SQLSource.JoinDefinition(
+            joinOp: .leftJoin,
+            leftSource: SQLSource.joined(SQLSource.JoinDefinition(
+                joinOp: .leftJoin,
+                leftSource: leftSource,
+                rightSource: middleSource,
+                onExpression: middleQuery.whereExpression,
+                mapping: annotationHavingExpression.annotation.association.middleAssociation.mapping(db))),
+            rightSource: rightSource,
+            onExpression: rightQuery.whereExpression,
+            mapping: annotationHavingExpression.annotation.association.rightAssociation.mapping(db)))
+        
+        // ... GROUP BY left.id
+        guard let leftTableName = leftQuery.source?.tableName else {
+            fatalError("Can't annotate tableless query")
+        }
+        let pkColumns = (try db.primaryKey(leftTableName)?.columns ?? (MiddleAssociation.LeftAssociated.selectsRowID ? [Column.rowID.name] : []))
+            .map { Column($0).qualified(by: leftQualifier) as SQLExpression }
+        guard !pkColumns.isEmpty else {
+            fatalError("Can't annotate table without primary key")
+        }
+        let joinedGroupByExpressions = pkColumns + leftQuery.groupByExpressions
+        
+        // Having: HAVING annotationExpression
+        let rightHavingExpression = try annotationHavingExpression.havingExpression(annotationHavingExpression.annotation.expression(db)).qualified(by: rightQualifier)
+        let joinedHavingExpression = (leftQuery.havingExpression.map { rightHavingExpression && $0 } ?? rightHavingExpression).qualified(by: rightQualifier)
+        
+        return try QueryInterfaceSelectQueryDefinition(
+            select: leftQuery.selection,
+            isDistinct: leftQuery.isDistinct,   // TODO: test
+            from: joinedSource,
+            filter: leftQuery.whereExpression,  // TODO: test
+            groupBy: joinedGroupByExpressions,
+            orderBy: leftQuery.orderings,       // TODO: test
+            isReversed: leftQuery.isReversed,   // TODO: test
+            having: joinedHavingExpression,
+            limit: leftQuery.limit)             // TODO: test
+            .prepare(db)
     }
 }
 
--- Tests/GRDBTests/HasManyThroughAssociationHasManyBelongsToHavingAnnotationTests.swift
+++ Tests/GRDBTests/HasManyThroughAssociationHasManyBelongsToHavingAnnotationTests.swift
@@ -7,6 +7,171 @@ import XCTest
     import GRDB
 #endif
 
+private typealias Country = HasManyThroughAssociationHasManyBelongsToFixture.Country
+private typealias Person = HasManyThroughAssociationHasManyBelongsToFixture.Person
+
 class HasManyThroughAssociationHasManyBelongsToHavingAnnotationTests: GRDBTestCase {
-    // TODO
+    
+    // TODO: conditions on middle table
+    
+    func testSimplestRequest() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThroughAssociationHasManyBelongsToFixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+            let graph = try Country
+                .filter(Country.citizens.count > 0)
+                .fetchAll(db)
+            
+            // TODO: check request & results
+            XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") > 0)")
+            
+            assertMatch(graph, [
+                ["code": "FR", "name": "France"],
+                ["code": "US", "name": "United States"],
+                ])
+        }
+    }
+    
+    func testLeftRequestDerivation() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThroughAssociationHasManyBelongsToFixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+            do {
+                // filter before leftJoined
+                let graph = try Country
+                    .filter(Column("code") != "FR")
+                    .filter(Country.citizens.count > 0)
+                    .fetchAll(db)
+                
+                XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") WHERE (\"countries\".\"code\" <> \'FR\') GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") > 0)")
+                
+                assertMatch(graph, [
+                    ["code": "US", "name": "United States"],
+                    ])
+            }
+            
+            do {
+                // filter after leftJoined
+                let graph = try Country
+                    .filter(Country.citizens.count > 0)
+                    .filter(Column("code") != "FR")
+                    .fetchAll(db)
+                
+                XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") WHERE (\"countries\".\"code\" <> \'FR\') GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") > 0)")
+                
+                assertMatch(graph, [
+                    ["code": "US", "name": "United States"],
+                    ])
+            }
+            
+            do {
+                // order before leftJoined
+                let graph = try Country
+                    .order(Column("name").desc)
+                    .filter(Country.citizens.count > 0)
+                    .fetchAll(db)
+                
+                XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") > 0) ORDER BY \"countries\".\"name\" DESC")
+                
+                assertMatch(graph, [
+                    ["code": "US", "name": "United States"],
+                    ["code": "FR", "name": "France"],
+                    ])
+            }
+            
+            do {
+                // order after leftJoined
+                let graph = try Country
+                    .filter(Country.citizens.count > 0)
+                    .order(Column("name").desc)
+                    .fetchAll(db)
+                
+                XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") > 0) ORDER BY \"countries\".\"name\" DESC")
+                
+                assertMatch(graph, [
+                    ["code": "US", "name": "United States"],
+                    ["code": "FR", "name": "France"],
+                    ])
+            }
+        }
+    }
+    
+    func testRightRequestDerivation() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThroughAssociationHasManyBelongsToFixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+            do {
+                // filtered citizens
+                let graph = try Country
+                    .filter(Country.citizens.filter(Column("name") != "Craig").count > 0)
+                    .fetchAll(db)
+                
+                XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON ((\"persons\".\"id\" = \"citizenships\".\"personId\") AND (\"persons\".\"name\" <> \'Craig\')) GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") > 0)")
+                
+                assertMatch(graph, [
+                    ["code": "FR", "name": "France"],
+                    ["code": "US", "name": "United States"],
+                    ])
+            }
+        }
+    }
+    
+    func testEqual() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThroughAssociationHasManyBelongsToFixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+            let graph = try Country
+                .filter(Country.citizens.count == 2)
+                .fetchAll(db)
+            
+            // TODO: check request & results
+            XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") = 2)")
+            
+            assertMatch(graph, [
+                ["code": "FR", "name": "France"],
+                ["code": "US", "name": "United States"],
+                ])
+        }
+    }
+    
+    func testIsEmpty() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThroughAssociationHasManyBelongsToFixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+            let graph = try Country
+                .filter(Country.citizens.isEmpty)
+                .fetchAll(db)
+            
+            // TODO: check request & results
+            XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") = 0)")
+            
+            assertMatch(graph, [
+                ["code": "DE", "name": "Germany"],
+                ])
+        }
+    }
+    
+    func testNotIsEmpty() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThroughAssociationHasManyBelongsToFixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+            let graph = try Country
+                .filter(!Country.citizens.isEmpty)
+                .fetchAll(db)
+            
+            // TODO: check request & results
+            XCTAssertEqual(lastSQLQuery, "SELECT \"countries\".* FROM \"countries\" LEFT JOIN \"citizenships\" ON (\"citizenships\".\"countryCode\" = \"countries\".\"code\") LEFT JOIN \"persons\" ON (\"persons\".\"id\" = \"citizenships\".\"personId\") GROUP BY \"countries\".\"code\" HAVING (COUNT(\"persons\".\"id\") <> 0)")
+            
+            assertMatch(graph, [
+                ["code": "FR", "name": "France"],
+                ["code": "US", "name": "United States"],
+                ])
+        }
+    }
 }
