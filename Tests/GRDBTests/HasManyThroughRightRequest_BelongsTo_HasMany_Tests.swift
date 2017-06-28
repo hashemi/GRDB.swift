25907ea2a2eb153aee58936ea0e0888105cb1158
Gwendal Roué <gr@pierlis.com>
2017-06-28 12:39:36 +0200

    HasManyThroughRightRequest<BelongsToAssociation, HasManyAssociation> tests

--- Tests/GRDBTests/HasManyThroughRightRequest_BelongsTo_HasMany_Tests.swift
+++ Tests/GRDBTests/HasManyThroughRightRequest_BelongsTo_HasMany_Tests.swift
@@ -14,116 +14,137 @@ class HasManyThroughRightRequest_BelongsTo_HasMany_Tests: GRDBTestCase {
     
     // TODO: conditions on middle table
     
-//    func testRequest() throws {
-//        let dbQueue = try makeDatabaseQueue()
-//        try HasManyThrough_BelongsTo_HasMany_Fixture().migrator.migrate(dbQueue)
-//        
-//        try dbQueue.inDatabase { db in
-//
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "FR")!
-//                let request = reader.makeRequest(Reader.books)
-//                let books = try request.fetchAll(db)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'FR\'))")
-//                assertMatch(books, [
-//                    ["id": 1, "name": "Arthur"],
-//                    ["id": 2, "name": "Barbara"],
-//                    ])
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "US")!
-//                let request = reader.makeRequest(Reader.books)
-//                let books = try request.fetchAll(db)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'US\'))")
-//                assertMatch(books, [
-//                    ["id": 2, "name": "Barbara"],
-//                    ["id": 3, "name": "Craig"],
-//                    ])
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "DE")!
-//                let request = reader.makeRequest(Reader.books)
-//                let books = try request.fetchAll(db)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'DE\'))")
-//                XCTAssertTrue(books.isEmpty)
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "US")!
-//                let request = reader.makeRequest(Reader.books).filter(Column("name") != "Craig")
-//                let books = try request.fetchAll(db)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'US\')) WHERE (\"books\".\"name\" <> \'Craig\')")
-//                assertMatch(books, [
-//                    ["id": 2, "name": "Barbara"],
-//                    ])
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "US")!
-//                let request = reader.makeRequest(Reader.books).order(Column("name").desc)
-//                let books = try request.fetchAll(db)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'US\')) ORDER BY \"books\".\"name\" DESC")
-//                assertMatch(books, [
-//                    ["id": 3, "name": "Craig"],
-//                    ["id": 2, "name": "Barbara"],
-//                    ])
-//            }
-//        }
-//    }
-//    
-//    func testFetchAll() throws {
-//        let dbQueue = try makeDatabaseQueue()
-//        try HasManyThrough_BelongsTo_HasMany_Fixture().migrator.migrate(dbQueue)
-//        
-//        try dbQueue.inDatabase { db in
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "FR")!
-//                let books = try reader.fetchAll(db, Reader.books)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'FR\'))")
-//                assertMatch(books, [
-//                    ["id": 1, "name": "Arthur"],
-//                    ["id": 2, "name": "Barbara"],
-//                    ])
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "US")!
-//                let books = try reader.fetchAll(db, Reader.books)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'US\'))")
-//                assertMatch(books, [
-//                    ["id": 2, "name": "Barbara"],
-//                    ["id": 3, "name": "Craig"],
-//                    ])
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "DE")!
-//                let books = try reader.fetchAll(db, Reader.books)
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'DE\'))")
-//                XCTAssertTrue(books.isEmpty)
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "US")!
-//                let books = try reader.fetchAll(db, Reader.books.filter(Column("name") != "Craig"))
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'US\')) WHERE (\"books\".\"name\" <> \'Craig\')")
-//                assertMatch(books, [
-//                    ["id": 2, "name": "Barbara"],
-//                    ])
-//            }
-//            
-//            do {
-//                let reader = try Reader.fetchOne(db, key: "US")!
-//                let books = try reader.fetchAll(db, Reader.books.order(Column("name").desc))
-//                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"bookships\" ON ((\"bookships\".\"bookId\" = \"books\".\"id\") AND (\"bookships\".\"readerCode\" = \'US\')) ORDER BY \"books\".\"name\" DESC")
-//                assertMatch(books, [
-//                    ["id": 3, "name": "Craig"],
-//                    ["id": 2, "name": "Barbara"],
-//                    ])
-//            }
-//        }
-//    }
+    func testRequest() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThrough_BelongsTo_HasMany_Fixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+
+            do {
+                let reader = try Reader.fetchOne(db, key: "arthur@example.com")!
+                let request = reader.makeRequest(Reader.books)
+                let books = try request.fetchAll(db)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" IS NULL))") // TODO: there should not be any request
+                XCTAssertTrue(books.isEmpty)
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "barbara@example.com")!
+                let request = reader.makeRequest(Reader.books)
+                let books = try request.fetchAll(db)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 1))")
+                assertMatch(books, [
+                    ["isbn": "book2", "title": "The Fellowship of the Ring", "libraryId": 1],
+                    ["isbn": "book3", "title": "Walden", "libraryId": 1],
+                    ["isbn": "book4", "title": "Le Comte de Monte-Cristo", "libraryId": 1],
+                    ])
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "craig@example.com")!
+                let request = reader.makeRequest(Reader.books)
+                let books = try request.fetchAll(db)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 2))")
+                assertMatch(books, [
+                    ["isbn": "book5", "title": "Querelle de Brest", "libraryId": 2],
+                    ["isbn": "book6", "title": "Eden, Eden, Eden", "libraryId": 2],
+                    ])
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "eve@example.com")!
+                let request = reader.makeRequest(Reader.books)
+                let books = try request.fetchAll(db)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 3))")
+                XCTAssertTrue(books.isEmpty)
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "barbara@example.com")!
+                let request = reader.makeRequest(Reader.books).filter(Column("title") != "Walden")
+                let books = try request.fetchAll(db)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 1)) WHERE (\"books\".\"title\" <> \'Walden\')")
+                assertMatch(books, [
+                    ["isbn": "book2", "title": "The Fellowship of the Ring", "libraryId": 1],
+                    ["isbn": "book4", "title": "Le Comte de Monte-Cristo", "libraryId": 1],
+                    ])
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "barbara@example.com")!
+                let request = reader.makeRequest(Reader.books).order(Column("title").desc)
+                let books = try request.fetchAll(db)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 1)) ORDER BY \"books\".\"title\" DESC")
+                assertMatch(books, [
+                    ["isbn": "book3", "title": "Walden", "libraryId": 1],
+                    ["isbn": "book2", "title": "The Fellowship of the Ring", "libraryId": 1],
+                    ["isbn": "book4", "title": "Le Comte de Monte-Cristo", "libraryId": 1],
+                    ])
+            }
+        }
+    }
+    
+    func testFetchAll() throws {
+        let dbQueue = try makeDatabaseQueue()
+        try HasManyThrough_BelongsTo_HasMany_Fixture().migrator.migrate(dbQueue)
+        
+        try dbQueue.inDatabase { db in
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "arthur@example.com")!
+                let books = try reader.fetchAll(db, Reader.books)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" IS NULL))") // TODO: there should not be any request
+                XCTAssertTrue(books.isEmpty)
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "barbara@example.com")!
+                let books = try reader.fetchAll(db, Reader.books)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 1))")
+                assertMatch(books, [
+                    ["isbn": "book2", "title": "The Fellowship of the Ring", "libraryId": 1],
+                    ["isbn": "book3", "title": "Walden", "libraryId": 1],
+                    ["isbn": "book4", "title": "Le Comte de Monte-Cristo", "libraryId": 1],
+                    ])
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "craig@example.com")!
+                let books = try reader.fetchAll(db, Reader.books)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 2))")
+                assertMatch(books, [
+                    ["isbn": "book5", "title": "Querelle de Brest", "libraryId": 2],
+                    ["isbn": "book6", "title": "Eden, Eden, Eden", "libraryId": 2],
+                    ])
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "eve@example.com")!
+                let books = try reader.fetchAll(db, Reader.books)
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 3))")
+                XCTAssertTrue(books.isEmpty)
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "barbara@example.com")!
+                let books = try reader.fetchAll(db, Reader.books.filter(Column("title") != "Walden"))
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 1)) WHERE (\"books\".\"title\" <> \'Walden\')")
+                assertMatch(books, [
+                    ["isbn": "book2", "title": "The Fellowship of the Ring", "libraryId": 1],
+                    ["isbn": "book4", "title": "Le Comte de Monte-Cristo", "libraryId": 1],
+                    ])
+            }
+            
+            do {
+                let reader = try Reader.fetchOne(db, key: "barbara@example.com")!
+                let books = try reader.fetchAll(db, Reader.books.order(Column("title").desc))
+                XCTAssertEqual(lastSQLQuery, "SELECT \"books\".* FROM \"books\" JOIN \"libraries\" ON ((\"libraries\".\"id\" = \"books\".\"libraryId\") AND (\"libraries\".\"id\" = 1)) ORDER BY \"books\".\"title\" DESC")
+                assertMatch(books, [
+                    ["isbn": "book3", "title": "Walden", "libraryId": 1],
+                    ["isbn": "book2", "title": "The Fellowship of the Ring", "libraryId": 1],
+                    ["isbn": "book4", "title": "Le Comte de Monte-Cristo", "libraryId": 1],
+                    ])
+            }
+        }
+    }
 }
